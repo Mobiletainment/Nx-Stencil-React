37150ef8b41102f057e73ee4ede95019986ce7a3
David Pertiller <david.pertiller@gmail.com>
2020-05-30 19:40:49 +0200

    fix(jest): add polyfill for CSSStyleSheet replace method which is needed for React specs when testing the wrapped web components

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
   },
+  setupFiles: ['construct-style-sheets-polyfill'],
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
